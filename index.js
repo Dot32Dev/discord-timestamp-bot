f6b2b3f1a53d9af359be54447a3e40a838854ae8
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2022-01-03 19:11:34 +0800

    ping/pong (except it's pig/pog)

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const token = require("./token.json")
 const intents = new Discord.Intents(32767)
 const client = new Discord.Client({ intents })
 
-console.log(token.token)
-client.login(token.token)
-
 let prefix = "!t"
+
+client.on("ready", () => console.log("Bot is online!"))
+client.on("messageCreate", message => {
+	console.log(message.content)
+
+	if (message.content.includes("pig")) {
+		message.reply(message.content.replace("pig", "pog").replace("@", "@ "))
+	}
+})
+
+client.login(token.token)
